23fbe5648b629a84c56537f2643c6f9a7c273163
ToniMaunde <31246918+ToniMaunde@users.noreply.github.com>
2017-09-15 16:50:09 +0200

    Projecto com Hibernate(4)

--- TPCDaFichaDeVectorEStack/README.md
+++
@@ -1,1 +0,0 @@
-# TPCDaFichaDeVectorEStack
--- src/fichadeclassesabstractastpc/modelohiber/TesteDeHibernate.java
+++ src/fichadeclassesabstractastpc/modelohiber/TesteDeHibernate.java
@@ -19,13 +19,22 @@ public class TesteDeHibernate {
         Session sessao = s.openSession();
         sessao.beginTransaction();
         
+        //Criando instancias do Tipo Professor
         Professor docente = new Professor();
         docente.setCadeira("POO");
         docente.setEstadoCivil("Casado");
         docente.setNome("Marco Cangela");
         docente.setDataDeNascimento(java.sql.Date.valueOf("1980-06-18"));
         
+        Professor docentee = new Professor();
+        docentee.setCadeira("EDA");
+        docentee.setDataDeNascimento(java.sql.Date.valueOf("1981-01-01"));
+        docentee.setEstadoCivil("Casado");
+        docentee.setNome("Herquiloide Hele");
+        
+        //Adicionando um professor a tabela de professores
         sessao.save(docente);
+        sessao.save(docentee);
         sessao.getTransaction().commit();
         sessao.close();
         s.close();
